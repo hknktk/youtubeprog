3a20b8b33706735abf1a30bcb6b4246d7a659f74
hknktk <75674444+hknktk@users.noreply.github.com>
2021-02-22 17:05:06 +0900

    commit
    更新した。

--- main.js
+++ main.js
@@ -27,4 +27,28 @@ document.getElementById("slideList").addEventListener("click",function(){
   })
 
 
+
+  const targetElement = document.querySelectorAll(".text");
+  document.addEventListener("scroll", function(){
+  for (let i = 0; i<targetElement.length; i++){
+    const getElementDistance =targetElement[i].
+    getBoundingClientRect().top + targetElement[i].clientHeight * .6
+    if(window.innerHeight > getElementDistance){
+      targetElement[i].classList.add("show");
+    }
+  }
+})
+
+  const targetElement2 = document.querySelectorAll(".detail>h2");
+  document.addEventListener("scroll", function(){
+  for (let i = 0; i<targetElement2.length; i++){
+    const getElementDistance =targetElement2[i].
+    getBoundingClientRect().top + targetElement2[i].clientHeight * .9
+    if(window.innerHeight > getElementDistance){
+      targetElement2[i].classList.add("show");
+    }
+  }
+})
+
+
 })
